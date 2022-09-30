2aab01c025d2a795520723cd27a60189b8a3c1fa
Timothy Molina <Rithium234@hotmail.com>
2022-09-30 16:22:36 -0400

    Added Components folder, Die component, and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import Die from "./Components/Die"
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='example'>
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+      <div className="content-box">
+        <main className="main-box">
+          <div className="dice-box">
+            <Die value="1" />
+            <Die value="2" />
+            <Die value="3" />
+            <Die value="4" />
+            <Die value="5" />
+            <Die value="6" />
+            <Die value="1" />
+            <Die value="2" />
+            <Die value="3" />
+            <Die value="4" />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
