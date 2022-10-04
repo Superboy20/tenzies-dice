998389fed4f58aea3470dcc351aa083304237d33
Timothy Molina <Rithium234@hotmail.com>
2022-10-04 17:19:17 -0400

    Revamped CSS styling to replace numbers with dots similar to actual dice, changes included adding a Dot and Face functions found within the Die component.

--- src/Components/Die.js
+++ src/Components/Die.js
@@ -1,12 +1,19 @@
 import React from "react"
 
+const Dot = () => <span className="dot" />;
+const Face = ({ children }) => <div className="die-front">{children}</div>;
 export default function Die(props) {
+    let dots = Number.isInteger(props.value) ? Array(props.value).fill(0)
+        .map((_, i) => <Dot key={i} />)
+        : null;
+
     return (
-        <div className="die-front"
+        <div className="bg-die-front"
             style={{ backgroundColor: props.isHeld ? "#8367c7" : "white" }}
             onClick={props.holdDice}
+            data-side={props.value}
         >
-            <h2 className="die-number">{props.value}</h2>
+            <Face>{dots}</Face>
         </div>
     )
 }
